9ab3c4bda69c46d799b7ce99737358f31eba99b7
Gentelmenski <msimonov781@gmail.com>
2025-06-29 21:24:01 +0300

    HTML file for testserver.go

--- testserver.go
+++ testserver.go
@@ -11,7 +11,23 @@ func (m msg) ServeHTTP(resp http.ResponseWriter, req *http.Request) {
 	fmt.Fprint(resp, m)
 }
 func main() {
-	msgHandler := msg("Hello world, test!!!")
+	/*msgHandler := msg("Hello world")
 	fmt.Println("Server is listening...")
-	http.ListenAndServe("localhost:8181", msgHandler)
+	http.ListenAndServe("localhost:8080", msgHandler)*/
+
+	/*fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	fmt.Println("Server is listening on http://localhost:8080")
+	http.ListenAndServe("localhost:8080", nil)*/
+
+	// Обработчик для статических файлов
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	// Обработчик для API-запроса
+	http.Handle("/api/message", msg("Hello world"))
+
+	fmt.Println("Server is listening on http://localhost:8080")
+	http.ListenAndServe("localhost:8080", nil)
 }
