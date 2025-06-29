3e2d835525c383acd2f520243a91e84fa5394f08
Gentelmenski <msimonov781@gmail.com>
2025-06-29 20:52:07 +0300

    цикл for

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	score := 111
+	/*score := 111
 	score = 50
 	test := "game over"
 
 	fmt.Println(test)
 	fmt.Println(score)
-	fmt.Println("Test")
+	fmt.Println("Test")*/
+	for i := 0; i < 11; i++ {
+		fmt.Println(i)
+	}
 }
